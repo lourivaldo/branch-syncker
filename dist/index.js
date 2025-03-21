63c4ed7346bbb79339577715f4fe318ca669bd84
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2025-03-21 20:40:38 -0300

    feat: build

--- dist/index.js
+++ dist/index.js
@@ -37987,13 +37987,27 @@ const core = __nccwpck_require__(2467);
 const github = __nccwpck_require__(8799);
 const simpleGit = __nccwpck_require__(7308);
 
+const git = simpleGit('.');
+
 async function run() {
-  const git = simpleGit('.');
   const status = await git.status();
   console.log(status)
+  const ORIGIN_BRANCH = core.getInput('ORIGIN_BRANCH');
+  const BRANCH = core.getInput('BRANCH');
+
+  await git.checkout(ORIGIN_BRANCH);
+  await git.fetch('origin');
+  await git.checkout(BRANCH);
+  await git.rebase(ORIGIN_BRANCH);
+  await git.push('origin', BRANCH, { '--force-with-lease': true });
+
+
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
+  // git checkout ${{ github.event.repository.default_branch }}
+  // git fetch origin
+  // git checkout hotfix
+  // git rebase ${{ github.event.repository.default_branch }}
+  // git push --force-with-lease
 }
 
 try {
@@ -38003,6 +38017,8 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   run()
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
--- index.js
+++ index.js
@@ -2,13 +2,27 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const simpleGit = require('simple-git');
 
+const git = simpleGit('.');
+
 async function run() {
-  const git = simpleGit('.');
   const status = await git.status();
   console.log(status)
+  const ORIGIN_BRANCH = core.getInput('ORIGIN_BRANCH');
+  const BRANCH = core.getInput('BRANCH');
+
+  await git.checkout(ORIGIN_BRANCH);
+  await git.fetch('origin');
+  await git.checkout(BRANCH);
+  await git.rebase(ORIGIN_BRANCH);
+  await git.push('origin', BRANCH, { '--force-with-lease': true });
+
+
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
+  // git checkout ${{ github.event.repository.default_branch }}
+  // git fetch origin
+  // git checkout hotfix
+  // git rebase ${{ github.event.repository.default_branch }}
+  // git push --force-with-lease
 }
 
 try {
@@ -18,6 +32,8 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   run()
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
