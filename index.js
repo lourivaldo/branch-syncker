4ee38b7054f87256efccfb05a9ee9f732f96d2e9
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2025-03-21 21:24:00 -0300

    feat: build

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const simpleGit = require('simple-git');
+const { IncomingWebhook } = require('@slack/webhook');
 
 const git = simpleGit('.');
 
-async function run() {
-  const status = await git.status();
-  // console.log(status)
-  const baseBranch = core.getInput('origin_branch');
+async function rebaseBranch() {
+  const baseBranch = core.getInput('origin-branch');
   const targetBranch = core.getInput('branch');
   console.log(`${baseBranch}->${targetBranch}`)
 
@@ -23,7 +22,25 @@ async function run() {
   } catch (error) {
     console.log('error')
     console.log(error)
+    throw new Error('Rebase conflict')
   }
+}
+
+async function notify() {
+  const url = core.getInput('slack-webhook');
+  const webhook = new IncomingWebhook(url);
+  await webhook.send({
+    // text: 'I\'ve got news for you...',
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "*:red_circle: GitHub Branch Sync [failed]*"
+        }
+      }
+    ]
+  });
 }
 
 try {
@@ -32,7 +49,10 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  run()
+  rebaseBranch()
+    .catch(() => {
+      notify()
+    })
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
   // // console.log(`The event payload: ${payload}`);
 } catch (error) {
